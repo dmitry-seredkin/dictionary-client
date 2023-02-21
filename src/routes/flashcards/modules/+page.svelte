7a777f08a301d2f2012a3c7871b76027fa264792
Dmitry Seredkin (laptop) <dimsf97@gmail.com>
2023-02-21 22:40:30 +0400

    add modules api requests

--- src/routes/flashcards/modules/+page.svelte
+++ src/routes/flashcards/modules/+page.svelte
@@ -5,19 +5,43 @@
 
   export let data: PageData;
 
+  let name = "";
+  $: ({ modules } = data);
+
+  const createModule = () => {
+    fetch("http://localhost:3000/api/v1/flashcards/modules", {
+      method: "POST",
+      body: JSON.stringify({ name }),
+    })
+      .then((response) => response.json())
+      .then((module) => (modules = modules.concat(module)));
+  };
+
   const getModulePath = (id: string): string => `/flashcards/modules/${id}`;
 </script>
 
-<ul class="list">
-  {#each data.modules as { id, name }}
-    <li>
-      <Link href={getModulePath(id)}>{name}</Link>
-    </li>
-  {/each}
-</ul>
+<section>
+  <header>
+    <input bind:value={name} />
+    <button class="add-button" on:click={createModule}>+</button>
+  </header>
+  <ul class="list">
+    {#each modules as { id, name }}
+      <li>
+        <Link href={getModulePath(id)}>{name}</Link>
+      </li>
+    {/each}
+  </ul>
+</section>
 
 <style lang="less">
   .list {
     display: grid;
+  }
+
+  .add-button {
+    @side: 1.5rem;
+    width: @side;
+    height: @side;
   }
 </style>
--- src/routes/flashcards/modules/[id]/+page.svelte
+++ src/routes/flashcards/modules/[id]/+page.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
   import type { PageData } from "./$types";
 
   export let data: PageData;
+
+  const deleteModule = () => {
+    fetch(`http://localhost:3000/api/v1/flashcards/modules/${data.id}`, {
+      method: "DELETE",
+    }).then(() => goto("/flashcards/modules"));
+  };
 </script>
 
 {#if data}
   <div>{JSON.stringify(data)}</div>
+  <button on:click={deleteModule}>Delete</button>
 {:else}
   <p>Entry isn't exist!</p>
 {/if}
