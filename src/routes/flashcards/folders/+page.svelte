05667a32decaa4df3632e76e4028bef4840782fe
Dmitry Seredkin (laptop) <dimsf97@gmail.com>
2023-02-21 21:34:26 +0400

    add folders api requests

--- src/routes/flashcards/folders/+page.svelte
+++ src/routes/flashcards/folders/+page.svelte
@@ -6,18 +6,36 @@
 
   export let data: PageData;
 
+  $: ({ folders } = data);
+  let name = "";
+
+  const createFolder = () => {
+    fetch("http://localhost:3000/api/v1/flashcards/folders", {
+      method: "POST",
+      body: JSON.stringify({ name }),
+    })
+      .then((response) => response.json())
+      .then((folder) => (folders = folders.concat(folder)));
+  };
+
   const getFolderPath = (id: string): string => `/flashcards/folders/${id}`;
 </script>
 
-<ul class="list">
-  {#each data.folders as folder}
-    <li>
-      <Link href={getFolderPath(folder.id)}>
-        <FolderItem {folder} />
-      </Link>
-    </li>
-  {/each}
-</ul>
+<section>
+  <header>
+    <input bind:value={name} />
+    <button class="add-button" on:click={createFolder}>+</button>
+  </header>
+  <ul class="list">
+    {#each folders as folder}
+      <li>
+        <Link href={getFolderPath(folder.id)}>
+          <FolderItem {folder} />
+        </Link>
+      </li>
+    {/each}
+  </ul>
+</section>
 
 <style lang="less">
   .list {
@@ -25,5 +43,11 @@
     grid-template-columns: repeat(auto-fit, minmax(6.25rem, 1fr));
     gap: 1rem;
     padding: 1.5rem; // TODO: variable
+  }
+
+  .add-button {
+    @side: 1.5rem;
+    width: @side;
+    height: @side;
   }
 </style>
--- src/routes/flashcards/folders/[id]/+page.svelte
+++ src/routes/flashcards/folders/[id]/+page.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
   import type { PageData } from "./$types";
 
   export let data: PageData;
+
+  const deleteFolder = () => {
+    fetch(`http://localhost:3000/api/v1/flashcards/folders/${data.id}`, {
+      method: "DELETE",
+    }).then(() => goto("/flashcards/folders"));
+  };
 </script>
 
 {#if data}
   <div>{JSON.stringify(data)}</div>
+  <button on:click={deleteFolder}>Delete</button>
 {:else}
   <p>Entry isn't exist!</p>
 {/if}
