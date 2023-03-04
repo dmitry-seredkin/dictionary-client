49d589a1bb63d6529a299f0b4d641b445761786a
Dmitry Seredkin (laptop) <dimsf97@gmail.com>
2023-03-04 14:07:27 +0400

    create entry-form entity; update search page

--- src/_routes/search/+page.svelte
+++ src/_routes/search/+page.svelte
@@ -1,56 +1,32 @@
 <script lang="ts">
+  import EntryForm, { type EntryFormValues } from "entities/entry-form.svelte";
   import { crawler, type CrawlerEntry } from "shared/api";
-  import { KeyName } from "shared/enums";
-  import { Button, Input } from "shared/ui";
+  import { Section } from "shared/ui";
+
 
   let entry: CrawlerEntry | null = null;
   let loading = false;
-  let query = "";
+  let form: EntryFormValues;
 
   const search = () => {
     loading = true;
     crawler
-      .search(query)
+      .search(form.text)
       .then((response) => (entry = response))
       .finally(() => (loading = false));
   };
-
-  const onKeyDown = ({ key }: KeyboardEvent) => {
-    if (key === KeyName.Enter) search();
-  };
 </script>
 
-<svelte:window on:keydown={onKeyDown} />
-<section class="container">
-  <h2 class="title">Search</h2>
-  <div class="search">
-    <Input bind:value={query} />
-    <Button on:click={search}>Search</Button>
-  </div>
+<Section title="New definitions search">
+  <EntryForm bind:form on:submit={search} />
   {#if loading}
     <p>Loading...</p>
   {:else if entry}
     <p class="entry">{JSON.stringify(entry, null, 2)}</p>
   {/if}
-</section>
+</Section>
 
 <style lang="less">
-  .container {
-    height: 100%;
-    padding: 1rem 1.5rem;
-    overflow: hidden;
-  }
-
-  .title {
-    margin-bottom: 1.5rem;
-  }
-
-  .search {
-    display: flex;
-    gap: 1rem;
-    margin-bottom: 2rem;
-  }
-
   .entry {
     white-space: pre;
   }
---
+++ src/entities/entry-form.svelte
@@ -0,0 +1,36 @@
+<script lang="ts" context="module">
+  export interface EntryFormValues {
+    separator: string;
+    text: string;
+  }
+</script>
+
+<script lang="ts">
+  import Button from "@smui/button";
+  import Textfield from "@smui/textfield";
+
+  export const form: EntryFormValues = { separator: "", text: "" };
+</script>
+
+<form class="form" on:submit>
+  <Textfield
+    bind:value={form.text}
+    label="Search for definitions"
+    style="width: 100%; grid-column: span 2"
+    input$rows={8}
+    textarea
+  />
+  <Textfield bind:value={form.separator} label="Separator" variant="outlined" />
+  <Button type="submit" style="height: 100%" variant="unelevated">Search</Button>
+</form>
+
+<style lang="less">
+  .form {
+    display: grid;
+    grid-template-columns: 1fr 15rem;
+    gap: 1.5rem; // TODO: variables
+    padding: 1rem; // TODO: variables
+    background-color: #ffffff; // TODO: variables
+    border-radius: 0.5rem; // TODO: variables
+  }
+</style>
